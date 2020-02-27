db3aa72d4588f2c557c6a07f33056edfe305ce35
alicia65 <alicia65>
2020-02-27 12:37:02 -0600

    Add project files.

---
+++ Weight on Mercury, Venus, and Mars/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Weight_on_Mercury__Venus__and_Mars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEarthWeight = new System.Windows.Forms.TextBox();
+            this.txtMercuryWeight = new System.Windows.Forms.TextBox();
+            this.txtVenusWeight = new System.Windows.Forms.TextBox();
+            this.txtMarsWeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEarthWeight
+            // 
+            this.txtEarthWeight.Location = new System.Drawing.Point(124, 38);
+            this.txtEarthWeight.Name = "txtEarthWeight";
+            this.txtEarthWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtEarthWeight.TabIndex = 0;
+            // 
+            // txtMercuryWeight
+            // 
+            this.txtMercuryWeight.Location = new System.Drawing.Point(367, 33);
+            this.txtMercuryWeight.Name = "txtMercuryWeight";
+            this.txtMercuryWeight.ReadOnly = true;
+            this.txtMercuryWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtMercuryWeight.TabIndex = 1;
+            // 
+            // txtVenusWeight
+            // 
+            this.txtVenusWeight.Location = new System.Drawing.Point(367, 105);
+            this.txtVenusWeight.Name = "txtVenusWeight";
+            this.txtVenusWeight.ReadOnly = true;
+            this.txtVenusWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtVenusWeight.TabIndex = 2;
+            // 
+            // txtMarsWeight
+            // 
+            this.txtMarsWeight.Location = new System.Drawing.Point(367, 178);
+            this.txtMarsWeight.Name = "txtMarsWeight";
+            this.txtMarsWeight.ReadOnly = true;
+            this.txtMarsWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtMarsWeight.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Earth";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(257, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mercury";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(247, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Venus";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(257, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mars";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(124, 168);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 257);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMarsWeight);
+            this.Controls.Add(this.txtVenusWeight);
+            this.Controls.Add(this.txtMercuryWeight);
+            this.Controls.Add(this.txtEarthWeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEarthWeight;
+        private System.Windows.Forms.TextBox txtMercuryWeight;
+        private System.Windows.Forms.TextBox txtVenusWeight;
+        private System.Windows.Forms.TextBox txtMarsWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Weight on Mercury, Venus, and Mars/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weight_on_Mercury__Venus__and_Mars
+{
+    public partial class Form1 : Form
+    {
+        SortedList<String, Double> WeightConversionFactors = new SortedList<string, double>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Initialize the SortedList
+            WeightConversionFactors.Add("Mercury", 0.3772);
+            WeightConversionFactors.Add("Venus", 0.9042);
+            WeightConversionFactors.Add("Mar", 0.3783);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double earthWeight = Convert.ToDouble(txtEarthWeight.Text);
+            
+            double mercuryWeight = earthWeight * WeightConversionFactors["Mercury"];
+            txtMercuryWeight.Text = mercuryWeight.ToString();
+
+            //TODO - can you finish Venus and Mars?
+
+            double venusConversionFactor = WeightConversionFactors["Venus"];
+            double venusWeight = earthWeight * venusConversionFactor;
+            txtVenusWeight.Text = venusWeight.ToString();
+
+            double marConversionFactor = WeightConversionFactors["Mar"];
+            double marWeight = earthWeight * marConversionFactor;
+            txtMarsWeight.Text = marWeight.ToString();
+
+        }
+    }
+}
